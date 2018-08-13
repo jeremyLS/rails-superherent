20ba08aa3c3ad64591362da95bc5a45db05af0cf
Thomas Delcros <delcrosthomas@gmail.com>
2018-08-13 21:57:59 +0200

    routes designed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,45 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :superheroes, only: [ :index, :show ] do
+    resources :bookings, only: [ :new, :create ]
+  end
+
+  resources :bookings, only: [ :new, :create, :index ]
+
+  resources :users, only: [ :new, :create ]
+
+  namespace :owner do
+    resources :superheroes, only: [ :new, :create, :index ]
+    resources :bookings, only: [ :index ] do
+      patch "validate", to: "bookings#validate"
+      patch "decline", to: "bookings#decline"
+    end
+  end
+
 end
+
+
+## SUPERHEROES
+# - get index
+# - get show
+
+## OWNER/SUPERHEROES
+# - get new
+# - post create
+# - get index
+
+## BOOKINGS
+# - get new
+# - post create
+# - get index
+
+## OWNER/BOOKINGS
+# - get index
+# - patch validate
+# - patch decline
+
+## USERS
+# - get new
+# - post create
