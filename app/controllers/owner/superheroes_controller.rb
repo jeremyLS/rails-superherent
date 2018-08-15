25141a9b106ed27c2da1b0abc16d78df8f280983
Thomas Delcros <delcrosthomas@gmail.com>
2018-08-15 15:51:33 +0200

    superhero-creation-form done

--- app/controllers/owner/superheroes_controller.rb
+++ app/controllers/owner/superheroes_controller.rb
@@ -8,6 +8,20 @@ class Owner::SuperheroesController < ApplicationController
   end
 
   def create
+    @superhero = Superhero.new(owner_superhero_params)
+    @owner = current_user
+    @superhero.owner = @owner
 
+    if @superhero.save
+      redirect_to superhero_path(@superhero)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def owner_superhero_params
+    params.require(:superhero).permit(:name, :address, :power, :description, :good, :daily_price, :photo)
   end
 end
