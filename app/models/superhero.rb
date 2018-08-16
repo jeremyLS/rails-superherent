9055e0323bbf398ff1706ed8470cf8b2716e2e5c
Thomas Delcros <delcrosthomas@gmail.com>
2018-08-16 16:14:15 +0200

    map and geocoder setup

--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,5 +1,11 @@
 class Superhero < ApplicationRecord
+  # CLOUDINARY
   mount_uploader :photo, PhotoUploader
+
+  # GEOCODER
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   has_many :bookings
   belongs_to :owner, class_name: "User"
 
